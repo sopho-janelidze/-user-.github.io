7d1488752234c1db198b28db8476ace0ef1efc42
sopho-janelidze <sopho.janelidze@gmail.com>
2024-03-12 12:54:05 -0700

    დავასრულე პირველი დავალება

---
+++ script.js
@@ -0,0 +1,26 @@
+let number = 100
+
+if ( number < 50) {
+    console.log ("less than 50");
+} else if (number > 20) {
+    console.log (" greater than 20");
+} else {
+    console.log ("error");
+}
+
+let userName = "mariam"
+
+let result = userName = "mariam" ? "true" : "False"
+
+console.log (result)
+
+
+
+const Name = "mariam";
+switch (Name) {
+    case "mariam" :
+    console.log ("true");
+    break;
+default:
+    console.log ("false")
+}
